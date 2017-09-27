203bd2af8a3757ca44e57785c5324caeadc1894e
FelixKrueger <felix.krueger@babraham.ac.uk>
2017-09-27 14:59:42 +0100

    Seems to be functional for tiled probes + and - seqs

--- make_stacked_reads.pl
+++ make_stacked_reads.pl
@@ -2,9 +2,9 @@
 use warnings;
 use strict;
 
-my $stepsize = 5000;
-my $readlength = 10000;
-my $number = 5;
+my $stepsize = 30;
+my $readlength = 100;
+my $number = 20;
 
 open (IN,$ARGV[0]) or die "Please give me a .fa file!\n";
 
@@ -16,13 +16,24 @@ while (<IN>){
     chomp;
     $seq .= $_;
 }
-warn "$id\n$seq\n";
+# warn "$id\n$seq\n";
 
 my $count = 0;
 my $offset = 0;
 while ($count < $number){
+    ++$count;
     my $tiled_seq = substr($seq,$offset,$readlength);
+    if ($count%2 ==0){
+	# reverse comeplementing the sequence
+	$tiled_seq = rc($tiled_seq);
+    }
     print ">seq$count\n$tiled_seq\n";
     $offset += $stepsize;
-    ++$count;
+}
+
+sub rc{ # reverse complements a sequence
+    my $seq = shift;
+    $seq = reverse $seq;
+    $seq =~ tr/CAGT/GTCA/;
+    return $seq;
 }
